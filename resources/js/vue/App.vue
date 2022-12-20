6b835613969937258f70f1c4db53378acb9dc942
XabierCarnero00 <xabigol4@gmail.com>
2022-12-20 13:13:20 +0100

    Creacion del proyecto

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Principal</h1>
+        <list/>
+    </div>
+</template>
+<script>
+import List from "./componets/List";
+export default {
+    components:{
+        List
+    }
+}
+</script>
