79f2409459944ce569294c002a6523fc573343b7
“Unai” <unaicabo15@gmail.com>
2022-12-22 12:37:19 +0100

    pagina principal casi terminada

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -9,7 +9,7 @@
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> --}}
     <title>pagina principal</title>
 
-    <header>
+    {{-- <header>
         <div class="caja-logo">
             <h1 class="logo">LOGO ERC</h1>
         </div>
@@ -25,7 +25,7 @@
                 <li class="col"><a href="">Login</a></li>
             </ul>
         </nav>
-    </header>
+    </header> --}}
 
 </head>
 <body>
@@ -40,31 +40,80 @@
 
         <img class="foto-tarjetas" src="/img/extorsion2.png" alt="foto">
         <div class="texto">
-            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+            <p>Un misterioso comerciante os ha robado la llave del cofre. Os amenaza con difundir todos vuestros trapos sucios por redes sociales si no jugáis al juego que os propone. </p>
         </div>
 
         <div class="botones">
             <button class="btn">Jugar</button>
         </div>
+        <p class="nivel">Basico</p>
+
     </div>
+
+
+        <div class="tarjeta">
+            <div class="titulo">
+                <h2>LA EXTORSIÓN DEL COMERCIO</h2>
+            </div>
+
+            <img class="foto-tarjetas" src="/img/extorsion2.png" alt="foto">
+            <div class="texto">
+                <p>Un misterioso comerciante os ha robado la llave del cofre. Os amenaza con difundir todos vuestros trapos sucios por redes sociales si no jugáis al juego que os propone. </p>
+            </div>
+
+            <div class="botones">
+                <button class="btn">Jugar</button>
+            </div>
+            <p class="nivel">Medio</p>
+        </div>
+
+
+
+        <div class="tarjeta">
+            <div class="titulo">
+                <h2>LA EXTORSIÓN DEL COMERCIO</h2>
+            </div>
+
+            <img class="foto-tarjetas" src="/img/extorsion2.png" alt="foto">
+            <div class="texto">
+                <p>Un misterioso comerciante os ha robado la llave del cofre. Os amenaza con difundir todos vuestros trapos sucios por redes sociales si no jugáis al juego que os propone. </p>
+            </div>
+
+            <div class="botones">
+                <button class="btn">Jugar</button>
+            </div>
+            <p class="nivel">Avanzado</p>
+        </div>
+
 </div>
 
-<div class="tarjetas">
-    <div class="tarjeta">
-        <div class="titulo">
-            <h2>LA EXTORSIÓN DEL COMERCIO</h2>
-        </div>
 
-        <img class="foto-tarjetas" src="/img/extorsion2.png" alt="foto">
-        <div class="texto">
-            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
-        </div>
-
-        <div class="botones">
-            <button class="btn">Jugar</button>
-        </div>
-    </div>
-</div>
 
 </body>
+
+    <nav id="nav-movil">
+      <ul class="flex-space-evenly-row">
+
+          <a href="/liburua-igo">
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+
+          </a>
+
+
+          <a href="/nagusia">
+            <i class="fa-solid fa-house"></i>
+            Hasiera
+          </a>
+
+
+
+          <a href="/profila">
+            <i class="fa-solid fa-user"></i>
+            Profila
+          </a>
+
+      </ul>
+    </nav>
+  </footer>
 </html>
