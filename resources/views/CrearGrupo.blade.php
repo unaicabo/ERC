4f6661a91e0647dd8ed29e88fc10273cf698712d
“Unai” <unaicabo15@gmail.com>
2023-01-13 12:55:46 +0100

    añadir integrante empezado

--- resources/views/CrearGrupo.blade.php
+++ resources/views/CrearGrupo.blade.php
@@ -7,29 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="./css/index.css">
     <title>Crear Grupo</title>
+    @vite(['resources/js/Grupo.js'])
 </head>
 
 <body id="fondo-explicacion">
-    <div>
+    {{-- <div>
         <h1 class="centrado-pruebas">CREAR GRUPO</h1>
+    </div> --}}
+    <div id="appForm" class="tarjeta-explicacion">
+
     </div>
-    <div class="tarjetas-explicacion">
-        <div class="tarjeta-explicacion">
 
 
-            <div class="titulo">
-                <p>EL JUEGO CONSISTE EN LEER EL ACERTIJO Y PENSAR LA RESPUESTA CORRECTA.</p>
-            </div>
-            <div class="texto">
-                Desde el lunes hasta el viernes, soy la ultima en llegar, el sabado soy la primera, y el domingo a descansar. ¿Quien soy?
-            </div>
-                <img class="img-acertijo" src="/img/dias-semana.png" alt="dias de la semana">
-            <div class="botones">
-                <input type="text" placeholder="Introducir respuesta">
-                <button class="btn">Validar</button>
-            </div>
-
-
-        </div>
 </body>
 </html>
