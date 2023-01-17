698f35307348710606e03edd48ec7de1c44dce35
XabierCarnero00 <xabigol4@gmail.com>
2023-01-17 11:36:38 +0100

    Commit candados

--- resources/js/vue/components/Candados.vue
+++ resources/js/vue/components/Candados.vue
@@ -2,13 +2,18 @@
     <div class="w-50">
         <h4>Progreso de candado:</h4>
         <div class="cajaProgesoCandados">
-            <div class="cajaLetra bg-primary text-white text-center display-6">7</div>
-            <div class="cajaLetra bg-primary text-white text-center display-6"></div>
-            <div class="cajaLetra bg-primary text-white text-center display-6"></div>
+            <div class="cajaLetra bg-primary text-white text-center display-6">{{ numUno }}</div>
+            <div class="cajaLetra bg-primary text-white text-center display-6">{{ numDos }}</div>
             <div class="cajaLetra bg-primary text-white text-center display-6"></div>
         </div>
     </div>
 </template>
+
+<script>
+   export default {
+    props: ['numUno','numDos']
+   }
+</script>
 
 <style>
     .cajaProgesoCandados {
