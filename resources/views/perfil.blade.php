ef429fb4f0e8dbd95ab575c6150582a8dec90aea
claragut <claragutc@gmail.com>
2023-01-25 12:12:57 +0100

    cl ult 25

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -11,7 +11,8 @@
 </head>
 <body id="pag-perfil">
 
-    <header>
+   
+ <header>
         <div class="caja-logo">
             <h1 class="logo">LOGO ERC</h1>
         </div>
@@ -77,7 +78,6 @@
             ?>
         </div>
     </main>
-
     
     <script src="https://kit.fontawesome.com/9d49876e0a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
