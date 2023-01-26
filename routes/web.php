e8b3ee888335e3b19383da802638d6ca90f6a1fa
asier <forpruebasclase@gmail.com>
2023-01-26 08:11:36 +0100

    cambio ruta principal

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-})->name('index');
+    return view('principal');
+})->name('principal');
 Route::get('/principal', function () {
     return view('principal');
 })->middleware('auth')->name('principal');
