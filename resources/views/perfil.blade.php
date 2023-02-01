eb0da725989fa076f4cddae12e64d31af0526f7a
asier <forpruebasclase@gmail.com>
2023-02-01 12:09:47 +0100

    cambiar bd

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -49,7 +49,7 @@
             <h2>Mis partidas</h2>
             <?php
                 use App\Http\Controllers\PruebasController;
-                
+
                 $partidaController = new PruebasController;
                 $partidas = $partidaController->partidasByUserId(Auth::user()->id);
                 foreach ($partidas as $key => $value) {
@@ -70,7 +70,7 @@
             ?>
         </div>
     </main>
-    
+
     <script src="https://kit.fontawesome.com/9d49876e0a.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
